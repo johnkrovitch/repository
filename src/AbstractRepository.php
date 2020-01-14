64d053a1c14f3bb2964f084036ebc05d66772483
johnkrovitch <arnaudfrezet@gmail.com>
2020-01-14 18:36:28 +0100

    Change manager registry to use one from the DoctrineBundle and add a getEntityManager method

--- src/AbstractRepository.php
+++ src/AbstractRepository.php
@@ -5,7 +5,8 @@ namespace JK\Repository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 
 abstract class AbstractRepository extends ServiceEntityRepository implements RepositoryInterface
 {
@@ -32,6 +33,11 @@ abstract class AbstractRepository extends ServiceEntityRepository implements Rep
     {
         $this->_em->persist($entity);
         $this->_em->flush();
+    }
+
+    public function getEntityManager(): EntityManagerInterface
+    {
+        return $this->_em;
     }
 
     protected function createCollection($results): Collection
